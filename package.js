253fdefb9ef44656be0d49394ee691c34878c2a0
SuccessMethodsLifestyle <adamlucifereves@gmail.com>
2026-03-06 00:23:43 +0000

    Create package.js

---
+++ package.js
@@ -0,0 +1,29 @@
+
+  "name": "eliteprofessionals",
+  "version": "1.0.0",
+  "private": true,
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start"
+  },
+  "dependencies": {
+    "next": "14.2.3",
+    "react": "^18",
+    "react-dom": "^18",
+    "bcryptjs": "^2.4.3",
+    "jsonwebtoken": "^9.0.2",
+    "better-sqlite3": "^9.4.3",
+    "cookie": "^0.6.0"
+  },
+  "devDependencies": {
+    "@types/node": "^20",
+    "@types/react": "^18",
+    "@types/react-dom": "^18",
+    "@types/bcryptjs": "^2.4.6",
+    "@types/jsonwebtoken": "^9.0.6",
+    "@types/better-sqlite3": "^7.6.10",
+    "@types/cookie": "^0.6.0",
+    "typescript": "^5"
+  }
+}
